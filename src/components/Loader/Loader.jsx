7f783a21e2d876c00aac20f91986abb1d6070e13
Judielyn Pagente <judielyn.pagente17@gmail.com>
2024-07-12 21:03:20 +0800

    Change loader to scale type

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,20 +1,39 @@
-import { MagnifyingGlass } from 'react-loader-spinner';
+// import { MagnifyingGlass } from 'react-loader-spinner';
+// import React from 'react';
+// import scss from './Loader.module.scss';
+// export const Loader = () => {
+//   return (
+//     <div className={scss.loader}>
+//       {' '}
+//       <MagnifyingGlass
+//         visible={true}
+//         height="80"
+//         width="80"
+//         ariaLabel="MagnifyingGlass-loading"
+//         wrapperStyle={{}}
+//         wrapperClass="MagnifyingGlass-wrapper"
+//         glassColor="#c0efff"
+//         color="#e15b64"
+//       />
+//     </div>
+//   );
+// };
+
+
 import React from 'react';
+import Scales from 'images/loader/loader.png';
+import SpinnerBase from 'images/loader/loader-mini.png';
 import scss from './Loader.module.scss';
+
 export const Loader = () => {
   return (
     <div className={scss.loader}>
-      {' '}
-      <MagnifyingGlass
-        visible={true}
-        height="80"
-        width="80"
-        ariaLabel="MagnifyingGlass-loading"
-        wrapperStyle={{}}
-        wrapperClass="MagnifyingGlass-wrapper"
-        glassColor="#c0efff"
-        color="#e15b64"
-      />
+      <div className={scss.spinnerContainer}>
+        <img src={Scales} alt="scales" className={scss.scales} />
+      </div>
+      <div className={scss.spinner}>
+        <img src={SpinnerBase} alt="spinner" className={scss.arrow} />
+      </div>
     </div>
   );
 };
