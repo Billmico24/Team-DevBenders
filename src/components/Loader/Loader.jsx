53d12e5c20011e3f8ee43990244fdb9d2d96de4a
Billmico24 <138019098+Billmico24@users.noreply.github.com>
2024-07-18 14:01:36 +0800

    commit all

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,44 +1,30 @@
-// import { ThemeContext } from 'components/Context/Context';
-// import React, { useContext } from 'react';
-// import { TailSpin } from 'react-loader-spinner';
-// import { LoaderWrapper, MainLoader } from './Loader.styled';
-
-// export const Loader = () => {
-//   const { isChristmas } = useContext(ThemeContext);
-
-//   return (
-//     <MainLoader>
-//       <LoaderWrapper>
-//         <TailSpin
-//           color={isChristmas ? '#D6001C' : '#FC842D'}
-//           arialLabel="loading-indicator"
-//           height="120"
-//           width="120"
-//           wrapperStyle={{}}
-//           wrapperClass=""
-//           visible={true}
-//           ariaLabel="three-circles-rotating"
-//           outerCircleColor=""
-//           innerCircleColor=""
-//           middleCircleColor=""
-//         />
-//       </LoaderWrapper>
-//     </MainLoader>
-//   );
-// };
-
-import React from 'react';
-import Scales from '../Loader/loader/images/loader.png';
-import SpinnerBase from '../Loader/loader/images/loader-mini.png';
+import { ThemeContext } from 'components/Context/Context';
+import React, { useContext } from 'react';
+import { TailSpin } from 'react-loader-spinner';
 import { LoaderWrapper, MainLoader } from './Loader.styled';
 
 export const Loader = () => {
+  const { isChristmas } = useContext(ThemeContext);
+
   return (
     <MainLoader>
-    <LoaderWrapper>
-        <img src={Scales} alt="scales" />
-        <img src={SpinnerBase} alt="spinner" />
-    </LoaderWrapper>
-  </MainLoader>
+      <LoaderWrapper>
+        <TailSpin
+          color={isChristmas ? '#FFFF' : '#FFFF'}
+          arialLabel="loading-indicator"
+          height="120"
+          width="120"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          ariaLabel="three-circles-rotating"
+          outerCircleColor=""
+          innerCircleColor=""
+          middleCircleColor=""
+        />
+      </LoaderWrapper>
+    </MainLoader>
   );
 };
+
+
