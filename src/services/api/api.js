df8880c9eaf5cbab9904782ca262ac8830d5bc25
Billmico24 <138019098+Billmico24@users.noreply.github.com>
2024-07-22 04:22:59 +0800

    axios.defaults.baseURL

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://slimmom-backend-team-devbenders.onrender.com;
-axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = 'https://team-devbenders-backend.onrender.com/';
+// axios.defaults.baseURL = 'http://localhost:3001';
 // const API_KEY = '';
 
 const END_POINTS = {
